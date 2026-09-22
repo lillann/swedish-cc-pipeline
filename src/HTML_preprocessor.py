4909892ebba4d80cb55680e5560cf98ab3397c39
lillann <ann.lilliestrom@svenska.gu.se>
2026-09-22 11:18:07 +0200

    HTML preprocessor update

--- src/HTML_preprocessor.py
+++ src/HTML_preprocessor.py
@@ -1,8 +1,13 @@
 import lxml.html
 import trafilatura
+import copy
 from datatrove.pipeline.base import PipelineStep
 from resiliparse.extract.html2text import extract_plain_text
 from resiliparse.parse.html import HTMLTree
+import ftfy
+import re
+
+EMPTY_LINES_REGEX = re.compile(r'\n\s*\n+', re.MULTILINE)
 
 
 def clean_html_trafilatura(raw_html):
@@ -25,14 +30,17 @@ def clean_html_trafilatura(raw_html):
         ],
     )
 
-
 def clean_html_lxml(raw_html):
-    if isinstance(raw_html, str):
-        raw_html = raw_html.encode("utf-8", errors="ignore")
+    
+  
+#    if isinstance(raw_html, str):
+#        raw_html = raw_html.encode("utf-8", errors="ignore")
 
     # Ta bort NULL-bytes som kraschar lxml
-    raw_html = raw_html.replace(b"\x00", b"")
-    parser = lxml.html.HTMLParser(recover=True, remove_comments=True)
+#    raw_html = raw_html.replace(b"\x00", b"")
+ 
+    
+    parser = lxml.html.HTMLParser(recover=True, remove_comments=False)
     try:
         doc = lxml.html.fromstring(raw_html, parser=parser)
     except (lxml.etree.ParserError, ValueError):
@@ -84,31 +92,48 @@ def clean_html_lxml(raw_html):
             except (KeyError, ValueError, lxml.etree.ParserError):
                 pass
 
-    # 4. Extrahera texten från det rensade trädet
-    extracted_text = " ".join(node.strip() for node in doc.itertext() if node.strip())
+  # 4. Extrahera texten från det rensade trädet
+  # extracted_text = " ".join(node.strip() for node in doc.itertext() if node.strip())
+  #  extracted_text = ftfy.fix_text(extracted_text)
     cleaned_html = lxml.html.tostring(doc, encoding="utf-8", method="html").decode("utf-8")
 
     metadata = {"link_count": link_count, "button_count": button_count}
 
-    return cleaned_html, extracted_text, metadata
-
-
-def clean_html_resiliparse(raw_html):
+    return cleaned_html,  metadata
+    
+    
+def extract_text_resiliparse(raw_html):
 
     tree = HTMLTree.parse(raw_html)
-    minimal_html = extract_plain_text(tree, preserve_formatting="minimal_html", main_content=False)
-    return minimal_html
+    text = extract_plain_text(tree, main_content=False) 
+    text = EMPTY_LINES_REGEX.sub('\n', text).strip()
+    return text
 
 
-class HTMLExtractor(PipelineStep):
+
+class TextExtractor(PipelineStep):
+  def __init__(self, **kwargs):
+      super().__init__(**kwargs)
+  
+  def run(self, data, rank: int = 0, world_size: int = 1):
+      for doc in data:
+        html = doc.text
+        html = ftfy.fix_text(html) # Fixar teckenfel
+        text = extract_text_resiliparse(html)
+        doc.metadata['html'] = html
+        doc.text = text
+        yield doc
+  
+
+class HTMLPreprocessor(PipelineStep):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def run(self, data, rank: int = 0, world_size: int = 1):
         for doc in data:
-            html = doc.text
+            html = doc.metadata['html']
 
-            cleaned_html, extracted_text, statistics = clean_html_lxml(html)
+            cleaned_html, statistics = clean_html_lxml(html)
             #  cleaned_html_resiliparse = clean_html_resiliparse(html)
             #  clenaed_html_trafilatura = clean_html_trafilatura(html)
 
@@ -117,5 +142,4 @@ class HTMLExtractor(PipelineStep):
             # doc.metadata['trafilatura'] = cleaned_html_trafilatura
             # doc.metadata['html_resiliparse'] ) =  cleaned_html_resiliparse
             doc.metadata["html"] = cleaned_html
-            doc.text = extracted_text
             yield doc
